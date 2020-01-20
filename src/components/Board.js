06b35000f1092f2180c4e39b5829661663c43ebb
kenneth <kenneth@huntingtonsMini.attlocal.net>
2020-01-19 21:13:38 -0500

    Components

---
+++ src/components/Board.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import List from './List'
+
+class Board extends React.Component {
+   render() {
+        return (
+            <div>
+            <p> board component</p>
+            <List/>
+            <List/>
+            <List/>
+            </div>
+
+        )
+
+   }
+
+}
+
+export default Board
